c9571d04da67224269b2e035f4867e709b14a7e3
hodorwho (LISE) <43373910+hodorwho@users.noreply.github.com>
2020-10-29 18:12:34 +0100

    updatesss + logo

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const hamburgerKnapp = document.getElementById('hamburgerKnapp');
 const navBar = document.getElementById('navBar');
+const toggle = document.getElementById('toggle');
 
 hamburgerKnapp.addEventListener('click', () => {
 //  console.log("button clicked");
   navBar.classList.toggle('open');
 })
+
+
+/*if $('navbar').toggle('open') {
+      $(this).toggleClass("toggle");
+    }
+    else {
+      $(this).toggleClass("hamburgerKnapp");
+    }
+});
+
+const toggle = document.getElementById('toggle');
+ $('toggle').toggleClass('toggle', open );
+
+ $('hamburgerKnapp').toggleClass(function (navBar, currentclass) {
+   if (navBar == open)
+
+ }
+ */
+
+ $(document).ready(function() {
+   $("hamburgerKnapp").click(function() {
+     $('hamburgerKnapp').toggleClass("toggle");
+   });
+ });
+
+ $("navBar").toggleClass("toggle");
+ // give up
 
 
 $('.degrees-knapp').click(function() {
